5532a95bef9c44d612cb6c1da6199429a9a29ffd
ZeeTom <zthom13@hotmail.com>
2021-07-27 16:26:20 -0600

    bug 2

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    this.makeChains(words);
   }
 
   /** set markov chains:
@@ -15,10 +13,26 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // MORE CODE HERE
+  makeChains(words) {
+    console.log(words);
+    let chains = new Map();
+    for (let i = 1; i <= words.length; i++) {
+      let prevWord = words[i - 1];
+      // console.log(prevWord);
+      let word = words[i];
+      // console.log(chains.get(prevWord));
+      // if (i === words.length) {
+      //   chains.get(prevWord).push(null);
+      // } else if (chains.has(prevWord)) {
+      //   chains.get(prevWord).push(word);
+      // } else {
+      chains.set(prevWord, word);
+      console.log("this is not working", chains.get(prevWord));
+      //   }
+    }
+    this.chains = chains;
+    return chains;
   }
-
 
   /** return random text from chains */
 
@@ -26,3 +40,10 @@ class MarkovMachine {
     // MORE CODE HERE
   }
 }
+
+// [the, cat, yes, hat, dif, the]
+
+// if hat in obj {
+// obj[hat].push(dif)
+// else
+// obj[yes] = [hat]
