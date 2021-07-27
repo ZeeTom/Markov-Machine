d489028d8f6e90e6b4024868987840f825a77bb9
Loni <kuangloni@gmail.com>
2021-07-27 15:44:53 -0700

    makeChains bug fix

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -14,24 +14,28 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains(words) {
-    console.log(words);
+    // console.log(words);
     let chains = new Map();
+
     for (let i = 1; i <= words.length; i++) {
       let prevWord = words[i - 1];
-      // console.log(prevWord);
       let word = words[i];
-      // console.log(chains.get(prevWord));
-      // if (i === words.length) {
-      //   chains.get(prevWord).push(null);
-      // } else if (chains.has(prevWord)) {
-      //   chains.get(prevWord).push(word);
-      // } else {
-      chains.set(prevWord, word);
-      console.log("this is not working", chains.get(prevWord));
-      //   }
+        
+      if (i === words.length) {
+        if (chains.has(prevWord)) {
+          chains.get(prevWord).push(null);
+        } else {
+          chains.set(prevWord, [null])
+        }
+      } else if (chains.has(prevWord)) {
+        chains.get(prevWord).push(word);
+      } else {
+        chains.set(prevWord, [word]);
+      }
     }
-    this.chains = chains;
-    return chains;
+    console.log(chains)
+    // this.chains = chains;
+    // return chains;
   }
 
   /** return random text from chains */
